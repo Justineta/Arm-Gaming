6a91c2aa72d349df28f150202dcc266a1fca1a78
Justineta <112271431+Justineta@users.noreply.github.com>
2023-03-25 17:32:05 +0100

    Create linux-native-included-games.sh

---
+++ linux-native-included-games.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+### Personal public script ###
+## Gaming with Armbian
+## @Justineta 
+
+## This script install common linux native games included in the repo
+sudo apt update
+sudo apt install xmoto openarena supertuxkart extremetuxracer torcs supertux alien-arena assaultcube sauerbraten nexuiz 0ad warzone2100 
